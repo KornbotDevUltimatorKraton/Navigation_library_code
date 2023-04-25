8eb477e4a19176f1e2e4189f00019c5cfb8d9765
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-04-25 17:51:24 +0700

    Add files via upload

--- navigation_lib.py
+++ navigation_lib.py
@@ -1,16 +1,49 @@
 import math 
+import serial 
 import requests 
 import subprocess 
+import struct
 import time 
 from itertools import count 
 
 wifi_list_rssi = {} 
+lidar_device = {} 
+pack_payload_lidar = {} 
 pack_payload = {} 
 def distance_RSSI(Pt,Pl,rssi):
         A = Pt-Pl 
         c_dat = (A-rssi)*0.014336239
         d = math.pow(10,c_dat) 
-        return d         
+        return d
+         
+def Lidar_info_report(email,lidar_name,unit_converter,serial_data):
+     for i in count(0):     
+            #print("Activating lidar system ",email+","+lidar_name) #Get the lidar system name 
+            data = serial_data.read(1)  # read the start byte
+            if data != b'\xFA':
+                continue  # start byte not found, skip this packet
+            data += serial_data.read(1)  # read the index byte
+            data += serial_data.read(2)  # read the speed bytes
+            data += serial_data.read(4)  # read the timestamp bytes
+            data += serial_data.read(360*3)  # read the data bytes (360 values of 3 bytes each)
+            data += serial_data.read(2)  # read the checksum bytes
+            # decode the data
+            data = data[2:-2]  # remove the index, speed, timestamp, and checksum bytes
+            ranges = []
+            angles = []
+            for i in range(0, len(data), 3):
+                     byte3, byte2, byte1 = struct.unpack('BBB', data[i:i+3])
+                     dist_mm = ((byte1 << 16) | (byte2 << 8) | byte3)
+                     range_m = dist_mm/unit_converter # convert mm to cm
+                     angle_deg = i / 3  # calculate the angle from the index
+                     angles.append(angle_deg)
+                     ranges.append(range_m)
+            #print the ranges and angles
+            #print("Ranges:", ranges,len(ranges))
+            #print("Angles:", angles,len(angles))
+            pack_payload_lidar[lidar_name] = {"Ranges":ranges,"Angles":angles} # Get the range and angle from the lidar to post back into the server 
+            lidar_device[email] = pack_payload_lidar
+            return lidar_device  
 
 def rssi_distance_report(email,wifi_name):  
   try:
@@ -34,6 +67,9 @@ def rssi_distance_report(email,wifi_name):
        return rssi_post_robot.json()    # Return the json value report to the back end data 
   except:
         print("Current wifi not found you are not connected")
-    
+def point_cloud_pcd_generator(email,camera_name):
+             print("Point cloud camera devices ",email,camera_name)  #Get the point cloud data to send into the server 
+              
+
    
 
