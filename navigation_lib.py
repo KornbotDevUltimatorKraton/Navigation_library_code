18873c4155ca9d013f88f926821779a49444a0a3
Chanapai Chuadchum <kornbot359@hotmail.com>
2023-05-06 12:25:16 +0700

    Add files via upload

--- navigation_lib.py
+++ navigation_lib.py
@@ -14,7 +14,13 @@ def distance_RSSI(Pt,Pl,rssi):
         c_dat = (A-rssi)*0.014336239
         d = math.pow(10,c_dat) 
         return d
-         
+def data_projection_post(email,payload_projection):
+        try:
+           post_projection = requests.post("http://0.0.0.0:5340/Multiplenode_logic",json={email:payload_projection}) # Get the data payload projection to run in real-time operating in mutlitasking function of sensor and actuator processing 
+           return post_projection.json() # return the projection data from the post request of the data projection 
+        except: 
+              print("Error communicating with the data projection server ")           
+
 def Lidar_info_report(email,lidar_name,unit_converter,serial_data):
      for i in count(0):     
             #print("Activating lidar system ",email+","+lidar_name) #Get the lidar system name 
@@ -78,12 +84,12 @@ def point_cloud_pcd_generator(camera_name,server_url,payload_command):
                       # Send the frame to the server
                       requests.post(server_url, json=payload_command) # Post request to the website 
 
-def Camera_image_sensor(email,project_name,camera_name,server_url,cam_index):
+def Camera_image_sensor(email,project_name,camera_name,cam_index,device_name):
              #Post the image from the camera input to the front end of the server   
-             
+             pos_mem_local = {}
              while True:
                       # Capture a frame from the video stream
-                      pos_mem_local = {}
+                      
                       ret, frame = camera_name.read()
                       # Check if the video has ended
                       if not ret:
@@ -101,17 +107,16 @@ def Camera_image_sensor(email,project_name,camera_name,server_url,cam_index):
                              pos_mem_local[project_name] = {'cam_index':cam_index,'message':data,'X':x,'Y':y}
                              # Draw the data on the frame
                              cv2.putText(frame, data, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                             #return {email:{project_name:pos_mem_local}}
                       # Encode the JPEG data in base64
                       jpeg_b64 = base64.b64encode(jpeg).decode("utf-8")
-                      print(jpeg_b64)
+                      #print(jpeg_b64)
+                      print({email:{device_name:pos_mem_local}})
+                      data_projection_post(email,{device_name:pos_mem_local})
                       # Send the frame to the server
-                      requests.post(server_url, json={email:{project_name:{"video":jpeg_b64,"position":pos_mem_local}}}) # Post request to the website 
-
-def data_projection_post(email,payload_projection):
-        try:
-           post_projection = requests.post("http://0.0.0.0:5340/Multiplenode_logic",json={email:payload_projection}) # Get the data payload projection to run in real-time operating in mutlitasking function of sensor and actuator processing 
-           return post_projection.json() # return the projection data from the post request of the data projection 
-        except: 
-              print("Error communicating with the data projection server ")                  
+                      requests.post('https://roboreactor.com/update_image', json={email:{project_name:{"video":jpeg_b64,"position":pos_mem_local}}}) # Post request to the website 
+                      
+                        # Return the data of the camera system positioning and project 
+                
    
 
